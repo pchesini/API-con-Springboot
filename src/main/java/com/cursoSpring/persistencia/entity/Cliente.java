4c997e25ecf28ef12aa8f791772d40f252b8439d
Pablo Chesini <pchesini1@gmail.com>
2024-09-11 15:20:48 -0300

    Creacion de clase cliente

--- src/main/java/com/cursoSpring/persistencia/entity/Cliente.java
+++ src/main/java/com/cursoSpring/persistencia/entity/Cliente.java
@@ -1,4 +1,20 @@
 package com.cursoSpring.persistencia.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="clientes")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String celular;
+    private String direccion;
+    @Column(name="correo_electronico")
+    private String  email;
+
+
 }
