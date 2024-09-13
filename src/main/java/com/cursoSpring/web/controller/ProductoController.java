01485e2355235271ae40e285d604be26e2df8394
Pablo Chesini <pchesini1@gmail.com>
2024-09-13 15:44:41 -0300

    Ceacion de controller de producto para manejo de peticiones http

--- src/main/java/com/cursoSpring/web/controller/ProductoController.java
+++ src/main/java/com/cursoSpring/web/controller/ProductoController.java
@@ -1,4 +1,41 @@
 package com.cursoSpring.web.controller;
 
+import com.cursoSpring.dominio.service.ProductoService;
+import com.cursoSpring.persistencia.entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/producto")
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoS;
+
+    @GetMapping("/all")
+    public List<Producto> getAll() {
+        return productoS.getAll();
+    }
+
+    @GetMapping("/idProducto")
+    public Optional<Producto> getProducto(@PathVariable("idProducto") int idProducto) {
+        return productoS.getProducto(idProducto);
+
+    }
+    @PostMapping("/save")
+    public Producto saveProducto(@RequestBody Producto producto){
+        return productoS.saveProducto(producto);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteProducto(@PathVariable("id") int idProducto){
+         productoS.deleteProducto(idProducto);
+    }
+    //@GetMapping("/categoria/idCategoria{}")
+    //public Optional<List<Producto>> getByCategoria(@PathVariable("idCategoria") int idCategoria ){
+     //   return productoS.getByCategoria(idCategoria);
+    //}
 }
+
